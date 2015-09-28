85377952c75c25b20877cf88fddbda0886ac0981
Asset <assets75@gmail.com>
2015-09-28 20:29:14 +0600

    Signed-off-by: Asset <assets75@gmail.com>

--- javascript-lib.js
+++ javascript-lib.js
@@ -2,5 +2,5 @@ calculateInOut = function(balanse,incoming,outgo){
     var _balanse = parseFloat(balanse),
         _incoming = parseFloat(Math.abs(incoming)),
         _outgo = parseFloat(Math.abs(outgo));
-    return _balanse+_incoming-_outgo;
+    return ((_balanse+_incoming).toFixed(10)-_outgo).toFixed(10);
 }
